12d7803d0a5f6b7eee33d8b1d106b94a617f83e3
MWSERVER\k-miyatani <k-miyatani@mediaware.co.jp>
2022-07-25 13:42:20 +0900

    chakraUIのテーマをカスタム

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,19 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ThemeProvider } from '@mui/material'
 import { createTheme } from '@mui/material/styles';
+import { extendTheme } from "@chakra-ui/react";
 
-export const theme = createTheme({
+export const chakraTheme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "gray.300",
+        color: "gray.800"
+      }
+    }
+  }
+});
+export const muiTheme = createTheme({
   palette: {
     primary: {
       main: '#ff8e88',
@@ -13,9 +24,11 @@ export const theme = createTheme({
 });
 function MyApp({ Component, pageProps }:AppProps) {
   return (
-      <ChakraProvider>
+    <ThemeProvider theme={muiTheme}>
+      <ChakraProvider theme={chakraTheme}>
         <Component {...pageProps} />
       </ChakraProvider>
+    </ThemeProvider>
   )
 }
 
